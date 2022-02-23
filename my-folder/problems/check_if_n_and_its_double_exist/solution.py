462ac9be46e9aae079f9a78a94485fe8d83da923
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-02-23 08:57:26 +0000

    Sync LeetCode submission - Check If N and Its Double Exist (python)

---
+++ my-folder/problems/check_if_n_and_its_double_exist/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def checkIfExist(self, arr):
+        for i in range(len(arr)):
+            for j in range (i+1,len(arr)):
+                if(arr[j]*2==arr[i] or arr[i]*2==arr[j]):
+                    return True
+        return False
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        
