96fcabc9d5c814e03a3d903fa00bb7deffd7346d
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-03-08 16:06:41 +0000

    Sync LeetCode submission - Valid Anagram (python)

---
+++ my-folder/problems/valid_anagram/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        return sorted(s)==sorted(t)
+        
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
