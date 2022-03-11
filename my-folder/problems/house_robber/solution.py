b03183cdc6a9590c9bab4c76613358d96f3d1bc0
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-03-11 08:36:29 +0000

    Sync LeetCode submission - House Robber (python)

---
+++ my-folder/problems/house_robber/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rob(self, nums):
+        last,now=0,0
+        for n in nums:
+            last,now=now,max(last+n,now)
+        return now
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
