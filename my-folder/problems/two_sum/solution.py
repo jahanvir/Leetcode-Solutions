6392ac38cd049cdd7ab352144b0fae7fc04f5ba2
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-09-04 17:30:36 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,16 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic={}
         for i in range(len(nums)):
             if target-nums[i] in dic:
                 return [dic[target-nums[i]],i]
             else:
                 dic[nums[i]]=i
-            
-        
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         
