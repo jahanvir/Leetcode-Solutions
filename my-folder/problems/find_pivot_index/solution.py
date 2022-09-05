de522127f43d8f71d840aefe9f4071798a0efcad
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-09-05 11:25:06 +0000

    Sync LeetCode submission - Find Pivot Index (python3)

--- my-folder/problems/find_pivot_index/solution.py
+++ my-folder/problems/find_pivot_index/solution.py
@@ -1,15 +1,10 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-    
-        total=0
-        for n in nums:
-            total+=n
-        total_left=0
+        total=sum(nums)
+        left=0
         for i in range(len(nums)):
-            if total_left==(total-nums[i]-total_left):
+            if left==total-nums[i]-left:
                 return i
-            total_left+=nums[i]
+            left+=nums[i]   
         return -1
-            
-            
         
