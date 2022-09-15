207361b169124c5a20ed525398c0b555a0adef4c
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-09-15 11:36:46 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (python3)

---
+++ my-folder/problems/binary_tree_inorder_traversal/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        self.traverse(root, ans)
+        return ans
+    
+    def traverse(self,root,ans):
+        if root==None:
+            return
+        self.traverse(root.left,ans)
+        ans.append(root.val)
+        self.traverse(root.right,ans)
+        
