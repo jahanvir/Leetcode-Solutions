3f870ace2b00663268b41247d311a5f95856b0fb
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-10-15 09:37:54 +0000

    Sync LeetCode submission - Length of Last Word (python3)

---
+++ my-folder/problems/length_of_last_word/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        
+        i=len(s)-1
+        count=0
+        while s[i]==' ':
+            i-=1
+        
+        while s[i]!=' ' and i>-1:
+            count+=1
+            i-=1
+        return count
+        
