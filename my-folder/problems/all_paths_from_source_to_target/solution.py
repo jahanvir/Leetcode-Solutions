407aab984a422d7dcad8ef35c73f773ded2d5599
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-12-30 16:18:05 +0000

    Sync LeetCode submission - All Paths From Source to Target (python3)

---
+++ my-folder/problems/all_paths_from_source_to_target/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        ans=[]
+        def dfs(u,path):
+            if u==len(graph)-1:
+                ans.append(path)
+                return
+            
+            for v in graph[u]:
+                dfs(v,path+[v])
+        
+        dfs(0,[0])
+        return ans
