263051d03e7e7297112941f0965e0d31fe001ffc
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-12-31 14:43:30 +0000

    Sync LeetCode submission - Is Subsequence (python3)

--- my-folder/problems/is_subsequence/solution.py
+++ my-folder/problems/is_subsequence/solution.py
@@ -1,17 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
-        point_s=0
-        point_t=0
-        while point_s<len(s) and point_t<len(t):
-            if s[point_s]==t[point_t]:
-                point_s+=1
-                point_t+=1
-            else:
-                point_t+=1
-        if point_s==len(s):
-            return True
-        else:
-            return False
-            
-        
+        si=0
+        ti=0
+        start=0
+        while si<len(s) and ti<len(t):
+            if s[si]==t[ti]:
+                si+=1
+
+            ti+=1
+        if si==len(s):
+            return True 
+        return False
