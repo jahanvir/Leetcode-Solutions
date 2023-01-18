304a5329abfdb18decaaad460604a5efcd399c24
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-01-18 15:11:20 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- my-folder/problems/maximum_subarray/solution.py
+++ my-folder/problems/maximum_subarray/solution.py
@@ -1,17 +1,14 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        
-        maxsum=float("-inf")
-        csum=float("-inf")
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxPossible= -100000
+        maxEnds=0
+
         for n in nums:
-            csum=max(n,csum+n)
-            maxsum=max(csum,maxsum)
-        return maxsum
-        
+            maxEnds+=n
+            if maxPossible<maxEnds:
+                maxPossible=maxEnds
             
-        
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+            if maxEnds<0:
+                maxEnds=0
+        return maxPossible
