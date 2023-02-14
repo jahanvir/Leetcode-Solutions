34cbf65170d82579891ee2f8540e20d036984ea9
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-02-14 14:06:29 +0000

    Sync LeetCode submission - Add Binary (python3)

--- my-folder/problems/add_binary/solution.py
+++ my-folder/problems/add_binary/solution.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def addBinary(self, a, b):
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+
         res=bin(int(a,2)+int(b,2))
         return res[2:]
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
-        
