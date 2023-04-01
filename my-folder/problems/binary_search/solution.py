ddb0c5f018cc1c1f7f6320c1bd29cb87a1349aab
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-04-01 08:48:18 +0000

    Sync LeetCode submission - Binary Search (python3)

--- my-folder/problems/binary_search/solution.py
+++ my-folder/problems/binary_search/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
         left=0
         right=len(nums)-1
-        if left==right and nums[0]==target:
-            return left   
-        while left<=right:
-            pivot=left+(right-left)//2
-            if nums[pivot]==target:
-                return pivot
-            if target<nums[pivot]:
-                right=pivot-1
-            else:
-                left=pivot+1
-        return -1
-            
-            
         
+
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+        return -1
+
