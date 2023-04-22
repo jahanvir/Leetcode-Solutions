766dfa8dff20cff021c6eb55c74b71feba9ffa2b
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-04-22 07:48:08 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (python3)

--- my-folder/problems/longest_palindromic_subsequence/solution.py
+++ my-folder/problems/longest_palindromic_subsequence/solution.py
@@ -1,14 +1,17 @@
 class Solution:
-  def longestPalindromeSubseq(self, s: str) -> int:
-    # dp(i, j) := LPS's length in s[i..j]
-    @functools.lru_cache(None)
-    def dp(i: int, j: int) -> int:
-      if i > j:
-        return 0
-      if i == j:
-        return 1
-      if s[i] == s[j]:
-        return 2 + dp(i + 1, j - 1)
-      return max(dp(i + 1, j), dp(i, j - 1))
-
-    return dp(0, len(s) - 1)
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n=len(s)
+        dp=[[0]*n for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][i]=1
+        
+        for w in range(1,n):
+            for i in range(n-w):
+                j=i+w
+                if s[i]==s[j]:
+                    dp[i][j]=dp[i+1][j-1] + 2
+                else:
+                    dp[i][j]=max(dp[i+1][j],dp[i][j-1])
+        return dp[0][n-1]
+        
