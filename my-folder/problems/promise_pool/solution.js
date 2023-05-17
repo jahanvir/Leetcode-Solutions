4862b601eda1a07682531c39094273a48241bd39
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-05-17 15:42:04 +0000

    Sync LeetCode submission - Promise Pool (javascript)

---
+++ my-folder/problems/promise_pool/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+var promisePool = async function(functions, n) {
+
+    let i=0;
+    async function callback(){
+        
+        if(i===functions.length){
+            return ;
+        }
+        await functions[i++]();
+        await callback();
+    }
+
+    const nPromises = Array(n).fill().map(callback);
+    await Promise.all(nPromises);
+    
+
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
