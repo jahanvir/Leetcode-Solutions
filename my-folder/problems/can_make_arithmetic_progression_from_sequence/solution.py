4151f41906bd43c0ddfa023dc18d17698132a5c3
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-06-06 15:16:41 +0000

    Sync LeetCode submission - Can Make Arithmetic Progression From Sequence (python3)

--- my-folder/problems/can_make_arithmetic_progression_from_sequence/solution.py
+++ my-folder/problems/can_make_arithmetic_progression_from_sequence/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        
         arr.sort()
-        diff=arr[0]-arr[1]
-        for i in range(1,len(arr)-1):
-            if arr[i]-arr[i+1]!=diff:
+        past=arr[0]
+        diff=arr[1]-arr[0]
+        for i in range(1,len(arr)):
+            if arr[i]-past != diff:
                 return False
+            past=arr[i]
         return True
-            
-        
