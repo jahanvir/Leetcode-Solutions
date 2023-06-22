2e1fff4efd134dc59bdcd64161ca64ef2303bf74
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-06-22 06:37:19 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (python3)

--- my-folder/problems/best_time_to_buy_and_sell_stock/solution.py
+++ my-folder/problems/best_time_to_buy_and_sell_stock/solution.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #pick the element i
-        #find maximum element in range i+1 to n
-        #store that differnce in d
-        #compare d in every itertaion and store max
-        #o(n^2)
-        n=len(prices)
-        diff_max=0
-        min_price=float("inf")
+
+        minVal=float("inf")
+        maxDiff=0
         for p in prices:
-            min_price=min(min_price,p)
-            diff_max=max(diff_max,p-min_price)
-        return diff_max
+            minVal=min(minVal,p)
+            maxDiff=max(maxDiff,p-minVal)
+        return maxDiff
+        
